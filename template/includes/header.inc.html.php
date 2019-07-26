d10d3733780ee3b588896e59e4421373aaea0130
celestetlg <53325047+celestetlg@users.noreply.github.com>
2019-07-25 20:10:28 -0700

    Add files via upload

---
+++ template/includes/header.inc.html.php
@@ -0,0 +1,15 @@
+<header>
+    <h1><span>Ace in the Hole</span> Sport Events</h1>
+</header>
+<nav>
+    <div class="toggle">
+        <i class="fa fa-bars menu"></i>
+    </div>
+    <ul>
+        <li><a class="current" href="#" >Home</a> </li>
+        <li><a href="#">Events</a> </li>
+        <li><a href="#">FAQ</a> </li>
+        <li><a href="#">Registration</a> </li>
+        <li><a href="#">Contact Us</a> </li>
+    </ul>
+</nav>
---
+++ template/template.html.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Celeste Gu">
+    <title>template</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Bungee+Outline|Heebo:700|Kanit:500i&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php include 'includes/header.inc.html.php'?>
+<main>
+    <section id="imageGallery">
+        <h3>image showcase goes here</h3>
+    </section>
+    <section id="section1">
+        <h3>Event introduction/event detail section 1</h3>
+        <p>paragraphparagraphparagraphparagraphparagraphparagraphparagraphparagraphparagraphparagraphparagraphparagraph
+            paragraphparagraphparagraphparagraphparagraphparagraphparagraphparagraphparagraph
+            paragraphparagraphparagraphparagraphparagraphparagraphparagraphparagraph</p>
+    </section>
+    <div class="section group">
+        <section id="section2" class="col span_6_of_12">
+            <h3>Portland weather forecast goes here/event detail section 2</h3>
+        </section>
+        <section id="section3" class="col span_6_of_12">
+            <h3>Social media feeds go here/event detail section 3</h3>
+        </section>
+    </div>
+</main>
+<?php include 'includes/footer.inc.html.php'?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/additional.js">
+
+</script>
+</body>
+
+</html>
